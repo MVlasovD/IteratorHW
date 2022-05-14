e432280b69de2fd06893f6e6c9b5d6daf161833a
Vlasov Mikhail <m.vlasov@dunice.net>
2022-05-14 14:25:36 +0300

    done

--- src/main/java/Randoms.java
+++ src/main/java/Randoms.java
@@ -1,24 +1,27 @@
+/**
+ * @author VMN
+ */
+
 import java.util.Iterator;
 import java.util.Random;
-import java.util.Spliterator;
-import java.util.function.Consumer;
-import java.util.stream.IntStream;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Randoms implements Iterable<Integer> {
 
     protected Random random;
-    static IntStream intStream;
+    private int min;
+    private int max;
+    static Integer tlrnd;
 
     public Randoms(int min, int max) {
         this.random = new Random();
-        intStream = random.ints(min,max + 1);
+        this.min = min;
+        this.max = max;
     }
 
     @Override
     public Iterator<Integer> iterator() {
         return new Iterator<Integer>() {
-            boolean isRandom = true;
-
             @Override
             public boolean hasNext() {
                 return true;
@@ -26,44 +29,9 @@ public class Randoms implements Iterable<Integer> {
 
             @Override
             public Integer next() {
-                int res = intStream.findFirst().getAsInt();
-                return res;
+                tlrnd = ThreadLocalRandom.current().nextInt(min, max + 1);
+                return tlrnd;
             }
         };
     }
-
-
-    public static int rnd(int min, int max) {
-        max -= min;
-        return (int) (Math.random() * ++max) + min;
-    }
-
-    @Override
-    public void forEach(Consumer<? super Integer> action) {
-        Iterable.super.forEach(action);
-    }
-
-    @Override
-    public Spliterator<Integer> spliterator() {
-        return Iterable.super.spliterator();
-    }
 }
-
-
-/**
- * Нужно написать класс, расширяющий Iterable<Integer>,
- * по которому мы будем итерироваться. В процессе итерирования мы будем от него
- * получать случайные числа в диапазоне значений. Диапазон задаётся двумя числами -
- * минимальным и максимальным значениями. Передаются они через параметры конструктора.
- * Таким образом, итератор по объектам нашего класса будет итерироваться по бесконечной
- * последовательности из случайных чисел в заданом интервале:
- * Реализация
- * Создайте класс Randoms, скопируйте его заготовку из кода выше.
- * Реализуйте требуемый интерфейсом Iterable метод, для чего вам может
- * понадобиться создать вспомогательный класс для итератора (реализующего Iterator<Integer>) по вашему классу.
- * Для генерации случайных чисел используйте встроенный класс Random.
- * Создайте класс Main, продемонстрируйте работосбособность написанного вами класса,
- * например как это было сделано выше.
- * Протестируйте работу программы. Не забывайте про правила форматирования кода
- * (для автоформата можете выделить код в идее и нажать Ctrl+Alt+L).
- **/
